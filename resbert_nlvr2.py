58bd23e0d868025b86b1b6ba6297dfd93d2b92ea
werywjw <werywjw@gmail.com>
2024-06-24 19:02:03 +0200

    added requirements & to main

--- resbert_nlvr2.py
+++ resbert_nlvr2.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/github/ngruenefeld/MLM-VQA-SoSe24/blob/main/resbert_nlvr2.ipynb
 """
 
-!pip install datasets torch transformers torchvision Pillow tqdm matplotlib numpy scikit-learn
 import torch
 from torch import nn
 from transformers import BertTokenizer, BertModel
@@ -110,100 +109,104 @@ class MultimodalModel(nn.Module):
 
         return torch.sigmoid(output)
 
-model = MultimodalModel().to(device)
-criterion = BCELoss()
-optimizer = Adam(model.parameters(), lr=5e-4)
+def main():
 
-epochs = 10
+    model = MultimodalModel().to(device)
+    criterion = BCELoss()
+    optimizer = Adam(model.parameters(), lr=5e-4)
 
-train_losses = []
-test_losses = []
-test_accuracies = []
-test_aucrocs = []
+    epochs = 10
 
-best_test_acc = 0.0 # add here
+    train_losses = []
+    test_losses = []
+    test_accuracies = []
+    test_aucrocs = []
 
-for epoch in range(epochs):
-    model.train()
-    total_train_loss = 0
-    train_progress_bar = tqdm(train_dataloader, desc=f'Epoch [{epoch+1}/{epochs}] Training')
+    best_test_acc = 0.0 # add here
 
-    for left_image, right_image, input_ids, attention_mask, labels in train_progress_bar:
-        # to device
-        left_image, right_image = left_image.to(device), right_image.to(device)
-        input_ids, attention_mask = input_ids.to(device), attention_mask.to(device)
-        labels = labels.to(device)
+    for epoch in range(epochs):
+        model.train()
+        total_train_loss = 0
+        train_progress_bar = tqdm(train_dataloader, desc=f'Epoch [{epoch+1}/{epochs}] Training')
 
-        optimizer.zero_grad()
-        outputs = model(left_image, right_image, input_ids, attention_mask)
-        loss = criterion(outputs.squeeze(), labels)
-        loss.backward()
-        optimizer.step()
-        total_train_loss += loss.item()
-
-    avg_train_loss = total_train_loss / len(train_dataloader)
-    train_losses.append(avg_train_loss)
-
-    model.eval()
-    correct, total = 0, 0
-    total_test_loss = 0
-    all_labels = []
-    all_outputs = []
-    test_progress_bar = tqdm(test_dataloader, desc=f'Epoch [{epoch+1}/{epochs}] Testing')
-
-    with torch.no_grad():
-        for left_image, right_image, input_ids, attention_mask, labels in test_progress_bar:
+        for left_image, right_image, input_ids, attention_mask, labels in train_progress_bar:
             # to device
             left_image, right_image = left_image.to(device), right_image.to(device)
             input_ids, attention_mask = input_ids.to(device), attention_mask.to(device)
             labels = labels.to(device)
 
+            optimizer.zero_grad()
             outputs = model(left_image, right_image, input_ids, attention_mask)
-            predicted = (outputs.squeeze() > 0.5).float()
             loss = criterion(outputs.squeeze(), labels)
-            total_test_loss += loss.item()
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            loss.backward()
+            optimizer.step()
+            total_train_loss += loss.item()
 
-            all_labels.extend(labels.cpu().numpy())
-            all_outputs.extend(outputs.squeeze().cpu().numpy())
+        avg_train_loss = total_train_loss / len(train_dataloader)
+        train_losses.append(avg_train_loss)
 
-    avg_test_loss = total_test_loss / len(test_dataloader)
-    avg_test_acc = correct / total
+        model.eval()
+        correct, total = 0, 0
+        total_test_loss = 0
+        all_labels = []
+        all_outputs = []
+        test_progress_bar = tqdm(test_dataloader, desc=f'Epoch [{epoch+1}/{epochs}] Testing')
 
-    test_losses.append(avg_test_loss)
-    test_accuracies.append(avg_test_acc)
+        with torch.no_grad():
+            for left_image, right_image, input_ids, attention_mask, labels in test_progress_bar:
+                # to device
+                left_image, right_image = left_image.to(device), right_image.to(device)
+                input_ids, attention_mask = input_ids.to(device), attention_mask.to(device)
+                labels = labels.to(device)
 
-    aucroc = roc_auc_score(all_labels, all_outputs)
-    test_aucrocs.append(aucroc)
+                outputs = model(left_image, right_image, input_ids, attention_mask)
+                predicted = (outputs.squeeze() > 0.5).float()
+                loss = criterion(outputs.squeeze(), labels)
+                total_test_loss += loss.item()
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-    print(f'Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}, Test Loss: {avg_test_loss:.4f}, Test Accuracy: {avg_test_acc:.4f}, Test AUC-ROC: {aucroc:.4f}')
+                all_labels.extend(labels.cpu().numpy())
+                all_outputs.extend(outputs.squeeze().cpu().numpy())
 
-    if avg_test_acc > best_test_acc:
-        best_test_acc = avg_test_acc
-        torch.save(model.state_dict(), 'resbert.pth') # change here
+        avg_test_loss = total_test_loss / len(test_dataloader)
+        avg_test_acc = correct / total
 
-epochs_range = range(1, epochs + 1)
+        test_losses.append(avg_test_loss)
+        test_accuracies.append(avg_test_acc)
 
-plt.figure(figsize=(12, 4))
+        aucroc = roc_auc_score(all_labels, all_outputs)
+        test_aucrocs.append(aucroc)
 
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, train_losses, label='Train Loss')
-plt.plot(epochs_range, test_losses, label='Test Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.title('Training and Test Loss')
-plt.legend()
+        print(f'Epoch [{epoch+1}/{epochs}], Train Loss: {avg_train_loss:.4f}, Test Loss: {avg_test_loss:.4f}, Test Accuracy: {avg_test_acc:.4f}, Test AUC-ROC: {aucroc:.4f}')
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, test_accuracies, label='Test Accuracy', color='green')
-plt.plot(epochs_range, test_aucrocs, label='Test AUC-ROC', color='red') #
-plt.xlabel('Epochs')
-plt.ylabel('Evaluation Metrics')
-plt.title('Test Accuracy and AUC-ROC')
-plt.legend()
+        if avg_test_acc > best_test_acc:
+            best_test_acc = avg_test_acc
+            torch.save(model.state_dict(), 'resbert.pth') # change here
 
-plt.tight_layout()
-plt.savefig('resbert_nlvr2.png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-plt.show()
+    epochs_range = range(1, epochs + 1)
 
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, train_losses, label='Train Loss')
+    plt.plot(epochs_range, test_losses, label='Test Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title('Training and Test Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, test_accuracies, label='Test Accuracy', color='green')
+    plt.plot(epochs_range, test_aucrocs, label='Test AUC-ROC', color='red') #
+    plt.xlabel('Epochs')
+    plt.ylabel('Evaluation Metrics')
+    plt.title('Test Accuracy and AUC-ROC')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('resbert_nlvr2.png', dpi=400, bbox_inches='tight', pad_inches=0.1)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
